a2380977d49f7a6fab55ad1de68d9302376d72e3
Jason Parker <Jbparker243@gmail.com>
2022-03-25 12:11:13 -0500

    added methodNotAllowed

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 const controller = require("./reservations.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:reservationId/status").put(controller.updateStatus);
+router
+  .route("/:reservationId/status")
+  .put(controller.updateStatus)
+  .all(methodNotAllowed);
 
-router.route("/:reservationId").get(controller.read).put(controller.update);
+router
+  .route("/:reservationId")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
-router.route("/").post(controller.create).get(controller.list);
+router
+  .route("/")
+  .post(controller.create)
+  .get(controller.list)
+  .all(methodNotAllowed);
 
 module.exports = router;
